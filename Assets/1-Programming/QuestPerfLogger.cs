79f4cd0271e8d457aef45570ad57a7f71a621050
Jaime Camacho <jaime.camacho.dev@gmail.com>
2025-09-10 16:36:59 +0200

    update

--- Assets/1-Programming/QuestPerfLogger.cs
+++ Assets/1-Programming/QuestPerfLogger.cs
@@ -27,7 +27,7 @@ public class QuestPerfLogger : MonoBehaviour
         if (_display == null)
         {
             var displays = new List<XRDisplaySubsystem>();
-            SubsystemManager.GetInstances(displays);
+            SubsystemManager.GetSubsystems(displays);
             if (displays.Count > 0)
                 _display = displays[0];
         }
